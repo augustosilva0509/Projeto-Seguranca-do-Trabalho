5bb4f9da0339fbaa03973a60d0afe2d68e7c9f68
augus <augus@AUGUSTO>
2025-11-04 17:39:46 -0300

    Adicionar arquivos de projeto.

---
+++ game/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gHeader = new System.Windows.Forms.GroupBox();
+            this.gTimerLbl = new System.Windows.Forms.Label();
+            this.gTimerLbl2 = new System.Windows.Forms.Label();
+            this.gTimerPict = new System.Windows.Forms.PictureBox();
+            this.gHeaderTitle = new System.Windows.Forms.Label();
+            this.gHeaderBack = new System.Windows.Forms.Label();
+            this.gRBar = new System.Windows.Forms.GroupBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.gRBarForm = new System.Windows.Forms.GroupBox();
+            this.gRBarFormBack = new System.Windows.Forms.Label();
+            this.gRBarFormBorder = new System.Windows.Forms.Label();
+            this.clOptions = new System.Windows.Forms.CheckedListBox();
+            this.gRBarBack = new System.Windows.Forms.Label();
+            this.gImage = new System.Windows.Forms.PictureBox();
+            this.gCenter = new System.Windows.Forms.GroupBox();
+            this.gImageDesc = new System.Windows.Forms.Label();
+            this.gCenterBack = new System.Windows.Forms.Label();
+            this.gHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gTimerPict)).BeginInit();
+            this.gRBar.SuspendLayout();
+            this.gRBarForm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gImage)).BeginInit();
+            this.gCenter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gHeader
+            // 
+            this.gHeader.Controls.Add(this.gTimerLbl);
+            this.gHeader.Controls.Add(this.gTimerLbl2);
+            this.gHeader.Controls.Add(this.gTimerPict);
+            this.gHeader.Controls.Add(this.gHeaderTitle);
+            this.gHeader.Controls.Add(this.gHeaderBack);
+            this.gHeader.Location = new System.Drawing.Point(1, 12);
+            this.gHeader.Name = "gHeader";
+            this.gHeader.Size = new System.Drawing.Size(1280, 108);
+            this.gHeader.TabIndex = 0;
+            this.gHeader.TabStop = false;
+            // 
+            // gTimerLbl
+            // 
+            this.gTimerLbl.Location = new System.Drawing.Point(1099, 71);
+            this.gTimerLbl.Name = "gTimerLbl";
+            this.gTimerLbl.Size = new System.Drawing.Size(100, 23);
+            this.gTimerLbl.TabIndex = 2;
+            this.gTimerLbl.Text = "label1";
+            // 
+            // gTimerLbl2
+            // 
+            this.gTimerLbl2.Location = new System.Drawing.Point(1118, 44);
+            this.gTimerLbl2.Name = "gTimerLbl2";
+            this.gTimerLbl2.Size = new System.Drawing.Size(100, 23);
+            this.gTimerLbl2.TabIndex = 0;
+            this.gTimerLbl2.Text = "label1";
+            // 
+            // gTimerPict
+            // 
+            this.gTimerPict.Location = new System.Drawing.Point(843, 26);
+            this.gTimerPict.Name = "gTimerPict";
+            this.gTimerPict.Size = new System.Drawing.Size(100, 50);
+            this.gTimerPict.TabIndex = 1;
+            this.gTimerPict.TabStop = false;
+            // 
+            // gHeaderTitle
+            // 
+            this.gHeaderTitle.BackColor = System.Drawing.Color.Transparent;
+            this.gHeaderTitle.Location = new System.Drawing.Point(166, 60);
+            this.gHeaderTitle.Name = "gHeaderTitle";
+            this.gHeaderTitle.Size = new System.Drawing.Size(44, 16);
+            this.gHeaderTitle.TabIndex = 3;
+            this.gHeaderTitle.Text = "label1";
+            // 
+            // gHeaderBack
+            // 
+            this.gHeaderBack.Location = new System.Drawing.Point(37, 24);
+            this.gHeaderBack.Name = "gHeaderBack";
+            this.gHeaderBack.Size = new System.Drawing.Size(100, 23);
+            this.gHeaderBack.TabIndex = 0;
+            this.gHeaderBack.Text = "label1";
+            // 
+            // gRBar
+            // 
+            this.gRBar.Controls.Add(this.btnNext);
+            this.gRBar.Controls.Add(this.gRBarForm);
+            this.gRBar.Controls.Add(this.clOptions);
+            this.gRBar.Controls.Add(this.gRBarBack);
+            this.gRBar.Location = new System.Drawing.Point(926, 140);
+            this.gRBar.Name = "gRBar";
+            this.gRBar.Size = new System.Drawing.Size(306, 395);
+            this.gRBar.TabIndex = 1;
+            this.gRBar.TabStop = false;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(0, 0);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 6;
+            this.btnNext.Text = "button1";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // gRBarForm
+            // 
+            this.gRBarForm.Controls.Add(this.gRBarFormBack);
+            this.gRBarForm.Controls.Add(this.gRBarFormBorder);
+            this.gRBarForm.Location = new System.Drawing.Point(49, 177);
+            this.gRBarForm.Name = "gRBarForm";
+            this.gRBarForm.Size = new System.Drawing.Size(200, 100);
+            this.gRBarForm.TabIndex = 8;
+            this.gRBarForm.TabStop = false;
+            // 
+            // gRBarFormBack
+            // 
+            this.gRBarFormBack.Location = new System.Drawing.Point(86, 53);
+            this.gRBarFormBack.Name = "gRBarFormBack";
+            this.gRBarFormBack.Size = new System.Drawing.Size(100, 23);
+            this.gRBarFormBack.TabIndex = 0;
+            this.gRBarFormBack.Text = "label1";
+            // 
+            // gRBarFormBorder
+            // 
+            this.gRBarFormBorder.Location = new System.Drawing.Point(0, 0);
+            this.gRBarFormBorder.Name = "gRBarFormBorder";
+            this.gRBarFormBorder.Size = new System.Drawing.Size(100, 23);
+            this.gRBarFormBorder.TabIndex = 1;
+            this.gRBarFormBorder.Text = "label1";
+            // 
+            // clOptions
+            // 
+            this.clOptions.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.clOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.clOptions.FormattingEnabled = true;
+            this.clOptions.Items.AddRange(new object[] {
+            "Elemento",
+            "Elemento",
+            "Elemento",
+            "Elemento",
+            "Elemento",
+            "Elemento",
+            "Elemento",
+            "Elemento",
+            "Elemento",
+            "Elemento"});
+            this.clOptions.Location = new System.Drawing.Point(16, 57);
+            this.clOptions.Name = "clOptions";
+            this.clOptions.Size = new System.Drawing.Size(133, 62);
+            this.clOptions.TabIndex = 5;
+            // 
+            // gRBarBack
+            // 
+            this.gRBarBack.Location = new System.Drawing.Point(0, 0);
+            this.gRBarBack.Name = "gRBarBack";
+            this.gRBarBack.Size = new System.Drawing.Size(100, 23);
+            this.gRBarBack.TabIndex = 7;
+            this.gRBarBack.Text = "label1";
+            // 
+            // gImage
+            // 
+            this.gImage.Location = new System.Drawing.Point(47, 21);
+            this.gImage.Name = "gImage";
+            this.gImage.Size = new System.Drawing.Size(667, 329);
+            this.gImage.TabIndex = 2;
+            this.gImage.TabStop = false;
+            // 
+            // gCenter
+            // 
+            this.gCenter.Controls.Add(this.gImageDesc);
+            this.gCenter.Controls.Add(this.gImage);
+            this.gCenter.Controls.Add(this.gCenterBack);
+            this.gCenter.Location = new System.Drawing.Point(50, 158);
+            this.gCenter.Name = "gCenter";
+            this.gCenter.Size = new System.Drawing.Size(743, 401);
+            this.gCenter.TabIndex = 3;
+            this.gCenter.TabStop = false;
+            // 
+            // gImageDesc
+            // 
+            this.gImageDesc.Location = new System.Drawing.Point(99, 375);
+            this.gImageDesc.Name = "gImageDesc";
+            this.gImageDesc.Size = new System.Drawing.Size(100, 23);
+            this.gImageDesc.TabIndex = 3;
+            this.gImageDesc.Text = "label1";
+            // 
+            // gCenterBack
+            // 
+            this.gCenterBack.Location = new System.Drawing.Point(470, 382);
+            this.gCenterBack.Name = "gCenterBack";
+            this.gCenterBack.Size = new System.Drawing.Size(100, 23);
+            this.gCenterBack.TabIndex = 4;
+            this.gCenterBack.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.gCenter);
+            this.Controls.Add(this.gRBar);
+            this.Controls.Add(this.gHeader);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gHeader.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gTimerPict)).EndInit();
+            this.gRBar.ResumeLayout(false);
+            this.gRBarForm.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gImage)).EndInit();
+            this.gCenter.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gHeader;
+        private System.Windows.Forms.GroupBox gRBar;
+        private System.Windows.Forms.PictureBox gImage;
+        private System.Windows.Forms.GroupBox gCenter;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label gImageDesc;
+        private System.Windows.Forms.Label gHeaderTitle;
+        private System.Windows.Forms.PictureBox gTimerPict;
+        private System.Windows.Forms.Label gTimerLbl;
+        private System.Windows.Forms.Label gHeaderBack;
+        private System.Windows.Forms.Label gRBarBack;
+        private System.Windows.Forms.Label gCenterBack;
+        private System.Windows.Forms.Label gTimerLbl2;
+        private System.Windows.Forms.GroupBox gRBarForm;
+        private System.Windows.Forms.Label gRBarFormBack;
+        private System.Windows.Forms.Label gRBarFormBorder;
+        private System.Windows.Forms.CheckedListBox clOptions;
+    }
+}
+
---
+++ game/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        // This will get the current WORKING directory(i.e. \bin\Debug)
+        public static string workingDirectory = Environment.CurrentDirectory;
+        // or: Directory.GetCurrentDirectory() gives the same result
+
+        // This will get the current PROJECT directory
+        public static string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName + "\\game";
+        public Form1()
+        {
+            InitializeComponent();
+            SetGame();
+            gTimerCounter();
+        }
+        public int gTimer = 30;
+        public async void gTimerCounter() {
+            if (gTimer == 0)
+            {
+                MessageBox.Show("Tempo esgotado!");
+                Application.Exit();
+            }
+                
+            await Task.Delay(1000);
+            gTimer--;
+            gTimerLbl.Text = $"{gTimer}";
+            gTimerCounter();
+        }
+        public void SetGame()
+        {
+            double resY = 720, resX = resY*16/9;
+            this.Size = new Size((int)(resX * 1.01), (int)(resY * 1.05));
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            #region Game Header
+            #region gHeader
+            gHeader.Size = new Size((int)(resX * 1.00), (int)(resY * 0.15));
+            gHeader.Location = new Point(0, 0);
+
+            gHeaderBack.Size = new Size((int)(resX * 1.00), (int)(resY * 0.15));
+            gHeaderBack.Location = new Point(0, 0);
+            gHeaderBack.Text = "";
+            gHeaderBack.BackColor = Color.FromArgb(228, 87, 87);
+            #endregion
+            #region gTitle
+            gHeaderTitle.Parent = gHeaderBack;
+            gHeaderTitle.Size = new Size((int)(resX * 0.52), (int)(resY * 0.15));
+            gHeaderTitle.Location = new Point((int)(resX * 0.08), 0);
+            gHeaderTitle.Font = new Font("Arial", (float)(resY*0.027), FontStyle.Bold);
+            gHeaderTitle.BackColor = Color.Transparent;
+            gHeaderTitle.ForeColor = Color.White;
+            gHeaderTitle.Text = "Caso 1- Escritório de Contabilidade";
+            gHeaderTitle.TextAlign = ContentAlignment.MiddleLeft;
+            #endregion
+            #region gTimer
+            gTimerPict.Parent = gHeaderBack;
+            gTimerPict.Size = new Size((int)(resX * 0.08), (int)(resY * 0.15));
+            gTimerPict.Location = new Point((int)(resX * 0.7), 0);
+            gTimerPict.Image = Image.FromFile(projectDirectory + "\\img\\hourglass.png");
+            gTimerPict.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            gTimerLbl2.Parent = gHeaderBack;
+            gTimerLbl2.Size = new Size((int)(resX * 0.05), (int)(resY * 0.12));
+            gTimerLbl2.Location = new Point((int)(resX * 0.846), 0);
+            gTimerLbl2.Font = new Font("Arial", (float)(resY * 0.027), FontStyle.Bold);
+            gTimerLbl2.TextAlign = ContentAlignment.BottomLeft;
+            gTimerLbl2.BackColor = Color.Transparent;
+            gTimerLbl2.ForeColor = Color.White;
+            gTimerLbl2.Text = "/30";
+
+            gTimerLbl.Parent = gHeaderBack;
+            gTimerLbl.Size = new Size((int)(resX * 0.09), (int)(resY * 0.15));
+            gTimerLbl.Location = new Point((int)(resX * 0.77), 0);
+            gTimerLbl.Font = new Font("Arial", (float)(resY * 0.072), FontStyle.Bold);
+            gTimerLbl.TextAlign = ContentAlignment.MiddleLeft;
+            gTimerLbl.BackColor = Color.Transparent;
+            gTimerLbl.ForeColor = Color.White;
+            gTimerLbl.Text = "30";
+            #endregion
+
+            #endregion
+
+            #region Game Right Bar
+            gRBar.Size = new Size((int)(resX * 0.3), (int)(resY * 0.85));
+            gRBar.Location = new Point((int)(resX * 0.7), (int)(resY * 0.15));
+
+            
+            gRBarBack.Size = new Size((int)(resX * 0.3), (int)(resY * 0.85));
+            gRBarBack.Location = new Point(0,0);
+            gRBarBack.BackColor = Control.DefaultBackColor;
+            gRBarBack.Text = "";
+
+            gRBarForm.Parent = gRBarBack;
+            gRBarForm.Location = new Point(0, (int)(resY * 0.07));
+            gRBarForm.Size = new Size((int)(resX * 0.25), (int)(resY * 0.56));
+
+            gRBarFormBorder.Location = new Point(0, 0);
+            gRBarFormBorder.Size = new Size((int)(resX * 0.25), (int)(resY * 0.56));
+            gRBarFormBorder.BackColor = Color.FromArgb(228, 87, 87);
+            gRBarFormBorder.Text = "";
+
+            gRBarFormBack.Location = new Point((int)(resX * 0.014*9/16), (int)(resY * 0.014));
+            gRBarFormBack.Size = new Size((int)(resX * 0.25 - gRBarFormBack.Location.X * 2), (int)(resY * 0.56 - gRBarFormBack.Location.Y * 2));
+            gRBarFormBack.BackColor = Color.FromArgb(217, 217, 217);
+            gRBarFormBack.Text = "";
+
+            clOptions.Parent = gRBarFormBack;
+            clOptions.Location = new Point((int)(resX * 0.005625), (int)(resY*0.02));
+            clOptions.Size = new Size(gRBarFormBack.Size.Width - clOptions.Location.X, gRBarFormBack.Size.Height - clOptions.Location.Y);
+            clOptions.Font = new Font("Arial", (float)(resY * 0.027), FontStyle.Bold);
+            clOptions.BackColor = Color.FromArgb(217, 217, 217);
+
+            btnNext.Parent = gRBarBack;
+            btnNext.Location = new Point((int)(resX * 0.005), (int)(gRBarForm.Size.Height + gRBarForm.Location.Y + resY * 0.05));
+            btnNext.Size = new Size((int)(resX * 0.24), (int)(resY * 0.1));
+            btnNext.BackColor = Color.FromArgb(107, 200, 118);
+            btnNext.FlatStyle = FlatStyle.Flat;
+            btnNext.FlatAppearance.BorderSize = 0;
+            btnNext.Text = "Próximo";
+            btnNext.Font = new Font("Arial", (float)(resY * 0.02), FontStyle.Bold);
+            #endregion
+
+            #region Game Center
+            gCenter.Size = new Size((int)(resX * 0.62), (int)(resY * 0.71));
+            gCenter.Location = new Point((int)(resX * 0.04), (int)(resY * 0.22));
+
+            gCenterBack.Size = new Size((int)(resX * 0.62), (int)(resY * 0.71));
+            gCenterBack.Location = new Point(0,0);
+            gCenterBack.BackColor = Control.DefaultBackColor;
+            gCenterBack.Text = "";
+
+            gImage.Parent = gCenterBack;
+            gImage.Size = new Size((int)(resX * 0.62), (int)(resY * 0.6));
+            gImage.Location = new Point(0, 0);
+            gImage.BackColor = Color.Cyan;
+
+            gImageDesc.Parent = gCenterBack;
+            gImageDesc.Location = new Point(0, (int)(gImage.Size.Height + gImage.Location.Y + resY * 0.02));
+            gImageDesc.Size = new Size((int)(resX * 0.28), (int)(resY * 0.05));
+            gImageDesc.Font = new Font("Arial", (float)(resY * 0.019), FontStyle.Bold);
+            gImageDesc.BackColor = Color.Transparent;
+            gImageDesc.ForeColor = Color.Black;
+            gImageDesc.Text = "Estado do projeto: Reprovado";
+            gImageDesc.TextAlign = ContentAlignment.MiddleLeft;
+
+            #endregion
+        }
+
+
+        
+    }
+}
